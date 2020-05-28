5c45d10cb300a23dfc5806a4ce14a9afcd8434e7
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-05-28 11:18:37 +0530

    adding flip feature

--- src/MyCard.js
+++ src/MyCard.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import "./CardStyle.css";
-import { FaBeer } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
+import ReactCardFlip from "react-card-flip";
 
 export default function MyCard() {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleClick = () => {
+    setFlipped(!flipped);
+  };
+
   return (
-    <Card
-      style={{ backgroundColor: "#8B78E6", height: "25vh" }}
-      className="text-center"
-    >
-      <p>
-        <FaBeer />
-      </p>
-    </Card>
+    <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+      <Card
+        style={{ backgroundColor: "#8B78E6", height: "25vh" }} 
+        className="text-center"
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        <p className="m-4 pt-4"></p>
+      </Card>
+
+      <Card
+        style={{ backgroundColor: "#d63031", height: "25vh" }} 
+        className="text-center"
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        <p className="m-4 pt-4">
+          <FaCode size="4rem" color="#FFF" />
+        </p>
+      </Card>
+    </ReactCardFlip>
   );
 }
