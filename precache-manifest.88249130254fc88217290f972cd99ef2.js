4a1ade06e629852b4655605a441f1cc43018d7c2
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-06-02 09:30:33 +0530

    Updates

---
+++ precache-manifest.88249130254fc88217290f972cd99ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2e7e2b012785f8185f960169bc16d1",
+    "url": "/card-memory-game/index.html"
+  },
+  {
+    "revision": "a41e0898fab57e1a06ec",
+    "url": "/card-memory-game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1dafc5e09c167ff9fdc1",
+    "url": "/card-memory-game/static/css/main.e22e5eed.chunk.css"
+  },
+  {
+    "revision": "a41e0898fab57e1a06ec",
+    "url": "/card-memory-game/static/js/2.159c81c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/card-memory-game/static/js/2.159c81c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dafc5e09c167ff9fdc1",
+    "url": "/card-memory-game/static/js/main.0823ba36.chunk.js"
+  },
+  {
+    "revision": "c0a30134dcf2c2a55d4a",
+    "url": "/card-memory-game/static/js/runtime-main.0d96fd06.js"
+  }
+]);
